8daafe3cf547a06c22e41f633d340a770fd7cfb0
Ante <anthel@github.com>
2020-10-22 13:49:14 +0200

    Working as intended

--- src/App.js
+++ src/App.js
@@ -1,7 +1,74 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './app.css';
 
+function generateTeams(list, teamSize) {
+  shuffle(list);
+
+  let counter = 0, 
+  teamList = [],
+  tempList = [];
+
+  for(let i = 0; i < list.length; i++) {
+    if(counter < teamSize) {
+      tempList.push(list[i]);
+      counter++;
+    }
+    if(counter === teamSize) {
+      teamList.push(tempList);
+      tempList = [];
+      counter = 0;
+    }
+  }
+  console.log(teamList)
+  return teamList;
+}
+
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
 function App() {
+
+  const [currentValue, setCurrentValue] = useState({name: ''});
+  const [participants, setParticipants] = useState([]);
+  const [teamlist, setTeamlist] = useState([]);
+
+  let teamSize = 3;
+
+  
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setCurrentValue({[name]: value})
+  }
+  const handleSubmit = (e) => {
+    setParticipants([...participants, currentValue.name]);
+    e.preventDefault();
+    setCurrentValue({name: ''})
+  }
+  const handleGenerate = (e) => {
+    const teams = generateTeams(participants.slice(), teamSize)
+    setTeamlist(teams);
+    console.log(teamlist)
+    e.preventDefault();
+  }
+  const renderTeams = () => {
+    
+  }
   return (
     <div className="layout">
       <div className="navbar">
@@ -22,7 +89,6 @@ function App() {
           className="softhouse-logo-image"
         />
         </div>
-        
       </div>
       <hr className="divider"/>
       <div className="content">
@@ -36,48 +102,11 @@ function App() {
             <hr className="content-divider"/>
             <div className="participants-content-container">
               <div className="participants-content-wrapper">
-                <p className="content-text">
-                  John Doe
-                </p>
-                <p className="content-text">
-                  John Doe
-                </p>
-                <p className="content-text">
-                  John Doe
-                </p>
-                <p className="content-text">
-                  John Doe
-                </p>
-                <p className="content-text">
-                  John Doe
-                </p>
-                <p className="content-text">
-                  John Doe
-                </p>
-                <p className="content-text">
-                  John Doe
-                </p>
-                <p className="content-text">
-                  John Doe
-                </p>
-                <p className="content-text">
-                  John Doe
-                </p>
-                <p className="content-text">
-                  John Doe
-                </p>
-                <p className="content-text">
-                  John Doe
-                </p>
-                <p className="content-text">
-                  John Doe
-                </p>
-                <p className="content-text">
-                  John Doe
-                </p>
-                <p className="content-text">
-                  John Doe
-                </p>
+                {participants.map((item, i) => 
+                  <p className="content-text" key={i}>
+                    {item}
+                  </p>
+                )}
               </div>
             </div>
           </div>
@@ -90,67 +119,54 @@ function App() {
             <hr className="content-divider"/>
             <div className="teams-content-container">
               <div className="teams-content-wrapper">
-                <div>
-                  <p className="content-text">
-                    John Doe
-                  </p>
-                  <p className="content-text">
-                    John Doe
-                  </p>
-                  <p className="content-text">
-                    John Doe
-                  </p>
-                  <p className="content-text">
-                    John Doe
-                  </p>
-                </div>
-                <div>
-                  <p className="content-text">
-                    John Doe
-                  </p>
-                  <p className="content-text">
-                    John Doe
-                  </p>
-                  <p className="content-text">
-                    John Doe
-                  </p>
-                  <p className="content-text">
-                    John Doe
-                  </p>
-                </div>
-                <div>
-                  <p className="content-text">
-                    John Doe
-                  </p>
-                  <p className="content-text">
-                    John Doe
-                  </p>
-                  <p className="content-text">
-                    John Doe
-                  </p>
-                  <p className="content-text">
-                    John Doe
-                  </p>
-                </div>
                 
+                <div>
+                  {teamlist.map(team => 
+                    <div>
+                      {team.map(member => 
+                        <p>
+                          {member}
+                        </p>
+                      )}
+                      <hr className="divider"/>
+                    </div>
+                    
+                  )}
+                </div>
               </div>
             </div>
           </div>
         </div>
-        <div className="formgroup">
-          <form>
+        <div className="bottom-container">
+          <div className="formgroup">
+            <h3>Add new participant</h3>
             <div className="formgroup-content">
-              <label for="firstname" className="content-text">Firstname: </label>
-              <input  label="Firstname" id="firstname" name="firstname"/>
+            <form>
+              <div className="formgroup-item">
+                <label for="name" className="content-text">Name: </label>
+                <input 
+                  label="Name" 
+                  id="name" 
+                  name="name"
+                  value={currentValue.name}
+                  onChange={handleChange}
+                />
+              </div>
+              
+              <button type="submit" 
+                className="formgroup-content-submit"
+                onClick={handleSubmit}
+                >Submit
+              </button>
+              <button 
+                className="generator-button"
+                onClick={handleGenerate}
+                >Generate
+              </button>
+            </form>
             </div>
-            <div className="formgroup-content">
-              <label for="lastname" className="content-text">Lastname: </label>
-              <input  label="lastname" id="lastname" name="lastname"/>
-            </div>
-            <button type="submit" className="formgroup-content-submit">Submit</button>
-          </form>
+          </div>
         </div>
-        
       </div>
     </div>
   );
